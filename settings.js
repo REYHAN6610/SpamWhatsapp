ce8eec2f21345f883443a367d857953c1beaecca
222-Xploit <rendyxword@gmail.com>
2025-01-27 08:19:33 +0700

    Add files via upload

---
+++ settings.js
@@ -0,0 +1,12 @@
+const fs = require('fs')
+
+global.owner = "xxxxx"
+global.footer = "xxxxx"
+
+let file = require.resolve(__filename)
+require('fs').watchFile(file, () => {
+  require('fs').unwatchFile(file)
+  console.log('\x1b[0;32m'+__filename+' \x1b[1;32mupdated!\x1b[0m')
+  delete require.cache[file]
+  require(file)
+})
